07a0f0463d7f125079e4a4e2b7b482c9fbb9ff6b
Devolution <contact@davidbouet.fr>
2019-01-06 18:40:09 +0100

    trivia is working. nneed to implement a few things mentionned in TODO

--- lidl_modules/async/trivia.js
+++ lidl_modules/async/trivia.js
@@ -7,36 +7,22 @@ require('console-success');
 let util = require('../../lidl_core/util.js');
 let mongoose = require('mongoose');
 let client = require('../../lidl_core/client.js');
+let triviaModel = require('../../lidl_core/models/trivia.js');
+
+//let trivia = require('../../lidl_ccore/models/trivia.js');
+
 class commandsWrapper{
 	constructor(){
 	}
 	// needed so the commands can be reloaded
 	init(){
-		let schema = mongoose.Schema({ question: String, response: String});	
-		let model
-			try {
-				model = mongoose.model('trivia');
-
-			} catch (error){
-				model = mongoose.model('trivia',schema);
-			}
-
 		this.initialized  = new Promise( (resolve,reject) => {
 	
 				var promise = () => { 
-				model.find({},'answer').lean().exec( (err, answer) => {
-						if (err){
-						reject(err);
-
-						}else{
-							console.log(answer)
-							let data = {
-								triviaCheat: triviaCheat
-							}	
-							resolve(data);
-						}
-						//		module.exports.commands = knownCommands;
-						});
+					let data = {
+						trivia: trivia
+					}
+					resolve(data);
 				};
 
 
@@ -74,6 +60,130 @@ function triviaCheat(target,obj,params,commandName){
 
 	
 }
+
+
+function trivia(target,obj,params,commandName){
+        
+	if (params.length && params.join(' ').trim() !== '' ){
+                if (params.includes('start')){
+				startTrivia(target,3);
+		}        
+	
+		//client.chat.on('PRIVMSG', forsenCDHandler);
+                
+                else if (params.includes('stop')){
+			client.chat.removeListener('PRIVMSG', forsenCDHandler);
+                }
+        }else{
+                util.sendMessage(target, "@" + obj.username + ", usage is !reload commands or !reload config");
+        }
+
+
+
+	//bot will log to stdout any messages sent even if we dont watch them
+}
+
+
+async function startTrivia(target, max){
+	let response;
+	// Fetch X questions & response
+	let query = triviaModel.find({},'-_id').lean().exec();
+	
+	query.then( (data) => { 
+		//console.log(data) 	
+
+		//shuffling the data using Fisher-Yate
+		
+		response = util.shuffle(data);
+		//console.log(response);
+
+
+
+
+	// Attach the listener that will listen to the answer (variable function)
+	// while i < max
+
+
+	// have to declare anonymous async function else we can't use AWAIT
+	(async () =>{ 	
+	for (let i = 0; i < max; i++){
+		// declaring this so the bot doesn't spam
+	/*	let sleep = async  ((ms = 0) => {
+			return new Promise ( r => setTimeout(r,ms));
+
+		});
+*/
+		let promise = () => { 
+			return new Promise ( (resolve, reject) => {
+			util.sendMessage(target,response[i].question);			
+
+			
+
+			let listener = ( (obj) => {
+				// if channel != the one running this trivia, we return
+				if (obj.channel !== target){
+					return;
+				}
+				
+				
+	
+//				console.log (response[i].response);
+				// if channel != thtis one we return
+
+				if (obj.message.toLowerCase().includes(response[i].response.toLowerCase())){
+					resolve(obj);
+				};				
+				
+
+			}); 
+			// register this listenerr
+			client.chat.on('PRIVMSG',listener);
+		
+			// after 5 sec, reject the promise and unregister the listener			
+			setTimeout( () => {
+				client.chat.removeListener('PRIVMSG',listener);
+				reject();
+			 }, 10000);
+// TODO: add a thing in config for time to answer trivia
+			});
+		}
+		await promise()
+			// user found the answer
+			.then( (obj) => {
+				util.sendMessage(target,"@" + obj.username + " is right, the answer was: '" + response[i].response + "'");
+		//		await sleep(3000);
+			})
+			// user didn't find it PepeLaugh
+			.catch( (error) => { 
+				util.sendMessage(target, "lol u guys suck the answer was '" + response[i].response + "'");
+		//		await sleep(3000);
+			});
+
+
+
+		}
+	})();
+	
+	// Promise to send the question and wait for the answer??? WutFaceW
+	// create listener in promise, wait for answer and destroy it? forsenE
+
+//	client.chat.on('PRIVMSG',listener); 
+	// Send the question
+	console.log(response[0]);
+
+
+	// Remove the question from the array
+	// Wait for X seconds elapser, or answer
+	// Repeat
+
+	// when done, remove the listener and send the scorers
+
+	});// end exec.then();		
+}
+
+function triviaLogic(){
+ //??
+} 
 
 
 
